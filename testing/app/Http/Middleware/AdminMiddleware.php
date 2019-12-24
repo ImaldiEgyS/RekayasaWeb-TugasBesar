f8a6f672d5f321e3ef44dc9f97f96eaf1cee4daa
ImaldiEgyS <173040106@mail.unpas.ac.id>
2019-12-23 20:20:28 -0800

    Multiple Login (Bug Fix User)

--- testing/app/Http/Middleware/AdminMiddleware.php
+++ testing/app/Http/Middleware/AdminMiddleware.php
@@ -20,7 +20,7 @@ class AdminMiddleware
         if(Auth::user()->usertype == 'admin') {
             return $next($request);
         } else {
-            return RouteServiceProvider::HOME;
+            return redirect('/');
         }
        
     }
